a7896e5625bdc168a2af7418108f7570a1beefe6
czechp <webcoderc@gmail.com>
2022-08-29 10:58:00 +0200

    Correct init script for docker image

--- docker/image/init.sh
+++ docker/image/init.sh
@@ -3,4 +3,4 @@ GIT_REPO=https://github.com/czechp/bsp-weight-monitoring-backend.git
 git clone $GIT_REPO /JavaProject
 cd JavaProject
 mvn clean install package -DskipTests
-java -jar --spring.datasource.username=weight-monitoring --spring.datasource.password=weight-monitoring123 --spring.profiles.active=production  /JavaProject/core/target/bsp-weight-monitoring.jar
+java -jar /JavaProject/core/target/bsp-weight-monitoring.jar --spring.datasource.username=weight-monitoring --spring.datasource.password=weight-monitoring123 --spring.profiles.active=production 
