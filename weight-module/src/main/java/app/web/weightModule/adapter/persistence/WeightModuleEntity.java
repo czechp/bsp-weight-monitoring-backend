b4b080d9b70596de0e21e839e8e49748c2c7bdf6
czechp <webcoderc@gmail.com>
2022-09-14 14:44:08 +0200

    Reformat code

--- weight-module/src/main/java/app/web/weightModule/adapter/persistence/WeightModuleEntity.java
+++ weight-module/src/main/java/app/web/weightModule/adapter/persistence/WeightModuleEntity.java
@@ -1,6 +1,8 @@
 package app.web.weightModule.adapter.persistence;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
--- weight-module/src/test/java/app/web/weightModule/domain/WeightModuleFactoryTest.java
+++ weight-module/src/test/java/app/web/weightModule/domain/WeightModuleFactoryTest.java
@@ -29,7 +29,7 @@ class WeightModuleFactoryTest {
     }
 
     @Test
-    void toWeightModuleDomainModuleStatusTest(){
+    void toWeightModuleDomainModuleStatusTest() {
         //given
         final var weightModuleEntity = WeightModuleTestProvider.entity();
         //when
@@ -41,7 +41,7 @@ class WeightModuleFactoryTest {
     }
 
     @Test
-    void toWeightModuleDomainProductionInfoTest(){
+    void toWeightModuleDomainProductionInfoTest() {
         //given
         final var weightModuleEntity = WeightModuleTestProvider.entity();
         //given
