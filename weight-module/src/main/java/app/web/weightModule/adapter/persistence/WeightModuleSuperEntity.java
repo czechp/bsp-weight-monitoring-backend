e3a1571465e3065e39c00fad8a60e33c67749f0f
czechp <webcoderc@gmail.com>
2022-09-14 13:03:27 +0200

    Weight module entity super class

---
+++ weight-module/src/main/java/app/web/weightModule/adapter/persistence/WeightModuleSuperEntity.java
@@ -0,0 +1,31 @@
+package app.web.weightModule.adapter.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+abstract class WeightModuleSuperEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    protected ProductionLineSimpleEntity productionLineSimpleEntity;
+
+    protected  float productUpRangeWeight;
+    protected  float productDownRangeWeight;
+    protected  int currentDosingDevice;
+    protected  float currentMeasure;
+    protected  boolean status;
+    protected  float totalMaterialWeight;
+    protected long totalProductPcs;
+    protected float correctProductPercent;
+}
