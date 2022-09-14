c89f1a7454aa2081ccfc4ca6c8dab752e06b4089
czechp <webcoderc@gmail.com>
2022-09-14 13:04:14 +0200

    Aimed weight module entity

---
+++ weight-module/src/main/java/app/web/weightModule/adapter/persistence/WeightModuleEntity.java
@@ -0,0 +1,37 @@
+package app.web.weightModule.adapter.persistence;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity()
+@Table(name = "weight_modules")
+@Getter
+@Setter
+@NoArgsConstructor
+class WeightModuleEntity extends WeightModuleSuperEntity {
+    public WeightModuleEntity(long id,
+                              ProductionLineSimpleEntity productionLineSimpleEntity,
+                              float productUpRangeWeight,
+                              float productDownRangeWeight,
+                              int currentDosingDevice,
+                              float currentWeight,
+                              boolean status,
+                              float totalMaterialWeight,
+                              long totalProductPcs,
+                              float correctProductPercent) {
+        super(id,
+                productionLineSimpleEntity,
+                productUpRangeWeight,
+                productDownRangeWeight,
+                currentDosingDevice,
+                currentWeight,
+                status,
+                totalMaterialWeight,
+                totalProductPcs,
+                correctProductPercent);
+    }
+}
