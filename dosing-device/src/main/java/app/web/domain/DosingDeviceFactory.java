a7b897921ca63f545e79b010d42ba47934ae1a65
czechp <webcoderc@gmail.com>
2022-10-14 09:56:43 +0200

    Factory for dosing device last

--- dosing-device/src/main/java/app/web/domain/DosingDeviceFactory.java
+++ dosing-device/src/main/java/app/web/domain/DosingDeviceFactory.java
@@ -1,7 +1,9 @@
 package app.web.domain;
 
 import app.web.adapter.DosingDeviceFirstEntity;
+import app.web.adapter.DosingDeviceLastEntity;
 import app.web.adapter.FirstModuleEntity;
+import app.web.adapter.LastModuleEntity;
 
 class DosingDeviceFactory {
     public static DosingDevice toDomain(DosingDeviceFirstEntity entity) {
@@ -23,7 +25,33 @@ class DosingDeviceFactory {
         );
     }
 
-    public static DosingDeviceFirstEntity toEntity(DosingDevice domain) {
+    public static DosingDevice toDomain(DosingDeviceLastEntity entity) {
+        return new DosingDeviceLast(
+                entity.getId(),
+                entity.getVersion(),
+                new ModuleInfo(entity.getLastModuleEntity().getId(), entity.getLineName()),
+                entity.getRecordNumber(),
+                entity.getTotalMaterial(),
+                new Measures(
+                        entity.getLastMeasure(),
+                        entity.getAmountBelowMeasures(),
+                        entity.getAmountCorrectMeasures(),
+                        entity.getAmountAboveMeasures(),
+                        entity.getAverageMeasure(),
+                        entity.getCorrectMeasurePercent()
+                )
+        );
+    }
+
+    public static DosingDeviceFirstEntity toEntity(DosingDeviceFirst domain) {
+        return createFirstDosingDeviceEntity(domain);
+    }
+
+    public static DosingDeviceLastEntity toEntity(DosingDeviceLast domain) {
+        return createLastDosingDeviceEntity(domain);
+    }
+
+    private static DosingDeviceFirstEntity createFirstDosingDeviceEntity(DosingDevice domain) {
         return new DosingDeviceFirstEntity(
                 domain.getId(),
                 domain.getVersion(),
@@ -39,4 +67,23 @@ class DosingDeviceFactory {
                 new FirstModuleEntity(domain.getModuleInfo().getModuleId())
         );
     }
+
+    private static DosingDeviceLastEntity createLastDosingDeviceEntity(DosingDevice domain) {
+        return new DosingDeviceLastEntity(
+                domain.getId(),
+                domain.getVersion(),
+                domain.getModuleInfo().getLineName(),
+                domain.getRecordNumber(),
+                domain.getTotalMaterial(),
+                domain.getMeasures().getLastMeasure(),
+                domain.getMeasures().getAmountBelowMeasures(),
+                domain.getMeasures().getAmountCorrectMeasures(),
+                domain.getMeasures().getAmountAboveMeasures(),
+                domain.getMeasures().getAverageMeasure(),
+                domain.getMeasures().getCorrectMeasurePercent(),
+                new LastModuleEntity(domain.getModuleInfo().getModuleId())
+        );
+    }
+
+
 }
--- dosing-device/src/main/java/app/web/domain/DosingDeviceLast.java
+++ dosing-device/src/main/java/app/web/domain/DosingDeviceLast.java
@@ -1,7 +1,7 @@
 package app.web.domain;
 
 class DosingDeviceLast extends DosingDevice {
-    public DosingDeviceLast(long id, long version, ModuleInfo moduleInfo, String lineName, ModuleType moduleType, int recordNumber, int totalMaterial, Measures measures) {
+    public DosingDeviceLast(long id, long version, ModuleInfo moduleInfo, int recordNumber, int totalMaterial, Measures measures) {
         super(id, version, moduleInfo, recordNumber, totalMaterial, measures);
     }
 
