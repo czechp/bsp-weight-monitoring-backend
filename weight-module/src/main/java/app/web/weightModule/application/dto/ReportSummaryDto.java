fedad179b71b25c0a16240467cd041af7d604e4e
czechp <webcoderc@gmail.com>
2022-10-19 12:54:41 +0200

    Make ReportSummary dto public

--- weight-module/src/main/java/app/web/weightModule/application/dto/ReportSummaryDto.java
+++ weight-module/src/main/java/app/web/weightModule/application/dto/ReportSummaryDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+public
 class ReportSummaryDto implements ReportSummary {
     private final long totalProductPcs;
     private final float totalMaterialWeight;
