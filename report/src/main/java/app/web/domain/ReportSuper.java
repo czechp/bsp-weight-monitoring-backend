7520f379bd38154187c54f8084f21eb1a68615f6
czechp <webcoderc@gmail.com>
2022-10-20 09:59:19 +0200

    Additional field to determine report creationd date

--- report/src/main/java/app/web/domain/ReportSuper.java
+++ report/src/main/java/app/web/domain/ReportSuper.java
@@ -2,16 +2,19 @@ package app.web.domain;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
 abstract class ReportSuper {
     private final long id;
     private final String lineName;
+    private final LocalDate localDate;
     private final WorkShift workShift;
     private final ReportSummary reportSummary;
     private final List<ReportDosingDeviceData> dosingDeviceFirstModule;
     private final List<ReportDosingDeviceData> dosingDeviceLastModule;
+
 
 
 }
