26343744003d7dae7047003d8ebbe9802a3fce13
czechp <webcoderc@gmail.com>
2022-10-20 09:58:17 +0200

    Make report class abstract

--- report/src/main/java/app/web/domain/ReportSuper.java
+++ report/src/main/java/app/web/domain/ReportSuper.java
@@ -1,14 +1,10 @@
 package app.web.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
-class Report {
-    private final long id = 0L;
-    private final String lineName = "";
+abstract class ReportSuper {
+    private final long id;
+    private final String lineName;
     private final WorkShift workShift;
     private final ReportSummary reportSummary;
     private final List<ReportDosingDeviceData> dosingDeviceFirstModule;
